5f6e14f127c0719527ced6cebf981b0d74c7ef53
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2017-01-19 17:54:13 +0100

    Removed console logging

--- src/SimpleMvcSitemap.Tests/XmlAssertionExtensions.cs
+++ src/SimpleMvcSitemap.Tests/XmlAssertionExtensions.cs
@@ -11,9 +11,6 @@ namespace SimpleMvcSitemap.Tests
     {
         public static void BeXmlEquivalent(this StringAssertions assertions, string filename)
         {
-            Console.WriteLine("Generatex XML");
-            Console.WriteLine(assertions.Subject);
-
             var fullPath = Path.Combine(ApplicationEnvironment.ApplicationBasePath, "Samples", filename);
             XDocument doc1 = XDocument.Parse(File.ReadAllText(fullPath));
             XDocument doc2 = XDocument.Parse(assertions.Subject);
